6caf36afee7669aa2ca52cda1b83f2c178e78d60
Eric Shannon <68311986+shonnon-zxs@users.noreply.github.com>
2025-04-07 15:42:08 +0800

    Delete CSS+conf directory

--- CSS+conf/tools/process.sh
+++
@@ -1,8 +0,0 @@
-# Process data
-
-python tools/create_dictionary.py
-python tools/create_dictionary_v1.py
-#python tools/compute_softscore.py v2
-#python tools/compute_softscore.py cp_v1
-#python tools/compute_softscore.py cp_v2
-
